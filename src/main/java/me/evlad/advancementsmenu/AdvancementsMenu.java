b2c4abc2814db325940d0ef0b437f8588a338649
evlad <emmanuelvlad@gmail.com>
2022-05-09 00:37:30 +0200

    change command name

--- src/main/java/me/evlad/advancementsmenu/AdvancementsMenu.java
+++ src/main/java/me/evlad/advancementsmenu/AdvancementsMenu.java
@@ -2,21 +2,15 @@ package me.evlad.advancementsmenu;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Level;
-
 import me.evlad.advancementsmenu.commands.AdvancementsMenuCommand;
 
 public final class AdvancementsMenu extends JavaPlugin {
 	@Override
 	public void onEnable() {
-		// Plugin startup logic
-		getLogger().log(Level.INFO, "Hello tout tle monde");
-
-		getCommand("success").setExecutor(new AdvancementsMenuCommand(this));
+		getCommand("advancements").setExecutor(new AdvancementsMenuCommand(this));
 	}
 
 	@Override
 	public void onDisable() {
-		// Plugin shutdown logic
 	}
 }
